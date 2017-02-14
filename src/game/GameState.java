197cca49f0733905c845c7a6e568de4c7410e5bf
Owen2284 <owen2284@googlemail.com>
2017-02-14 11:28:31 +0000

    Continued work on methods.

--- src/game/GameState.java
+++ src/game/GameState.java
@@ -16,12 +16,19 @@ public class GameState {
 	private int turnNumber;
 	
 	private final int BOARD_SIZE = 8;
+	
+	public static final int COUNTER_DARK = 1;
+	public static final int COUNTER_LIGHT = 2;
 
 	/*
 	 * Basic constructor.
 	 */
 	public GameState(Player p1, Player p2) {
 		this.board = new int[BOARD_SIZE][BOARD_SIZE];
+		this.board[(BOARD_SIZE / 2) - 1][(BOARD_SIZE / 2) - 1] = COUNTER_LIGHT;
+		this.board[(BOARD_SIZE / 2)][(BOARD_SIZE / 2) - 1] = COUNTER_DARK;
+		this.board[(BOARD_SIZE / 2) - 1][(BOARD_SIZE / 2)] = COUNTER_DARK;
+		this.board[(BOARD_SIZE / 2)][(BOARD_SIZE / 2)] = COUNTER_LIGHT;
 		this.players = new Player[2];
 		this.players[0] = p1;
 		this.players[1] = p2;
@@ -36,6 +43,7 @@ public class GameState {
 		this.players = new Player[2];
 		this.players[0] = that.getPlayer(0);
 		this.players[1] = that.getPlayer(1);
+		this.turnNumber = that.getTurnNumber();
 	}
 	
 	/*
@@ -90,8 +98,33 @@ public class GameState {
 	 * Returns the number of turns remaining before the end of the game.
 	 */
 	public int getTurnsLeft() {
-		int totalTurns = (BOARD_SIZE * BOARD_SIZE) - 4;
-		return
+		int totalTurns = getMaxTurns();
+		return totalTurns - this.turnNumber;
+	}
+	
+	/*
+	 * Gets the value located at a specific point of the game board.
+	 */
+	public int getBoardValue(int row, int col) {
+		try {
+			return this.board[row][col];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			throw new ArrayIndexOutOfBoundsException("The game board is of size " + BOARD_SIZE + " by " + BOARD_SIZE + ". The coordinates entered are out of these bounds.");
+		}
+	}
+	
+	/*
+	 * Returns a boolean array of where the provided player can play their counters.
+	 */
+	public boolean[][] getLegalMoves(Player p) {
+		boolean[][] legalMoves = new boolean[BOARD_SIZE][BOARD_SIZE];
+		for (int row = 0; row < BOARD_SIZE; ++row) {
+			for (int col = 0; col < BOARD_SIZE; ++col) {
+				int boardValue = getBoardValue(row, col);
+				legalMoves[row][col] = false;
+			}
+		}
+		return legalMoves;
 	}
 
 }
