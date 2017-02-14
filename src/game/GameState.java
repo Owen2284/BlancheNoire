1e09f688752f5f23ce5c6a7464c82ffe0576cb8c
Owen <owen2284@googlemail.com>
2017-02-14 18:30:50 +0000

    More work.

--- src/game/GameState.java
+++ src/game/GameState.java
@@ -25,6 +25,11 @@ public class GameState {
 	 */
 	public GameState(Player p1, Player p2) {
 		this.board = new int[BOARD_SIZE][BOARD_SIZE];
+		for (int row = 0; row < BOARD_SIZE; ++row) {
+			for (int col = 0; col < BOARD_SIZE; ++col) {
+				this.board[row][col] = 0;
+			}
+		}
 		this.board[(BOARD_SIZE / 2) - 1][(BOARD_SIZE / 2) - 1] = COUNTER_LIGHT;
 		this.board[(BOARD_SIZE / 2)][(BOARD_SIZE / 2) - 1] = COUNTER_DARK;
 		this.board[(BOARD_SIZE / 2) - 1][(BOARD_SIZE / 2)] = COUNTER_DARK;
@@ -114,17 +119,149 @@ public class GameState {
 	}
 	
 	/*
+	 * 
+	 */
+	public int[] getBoardDims() {
+		int[] dims = {BOARD_SIZE, BOARD_SIZE};
+		return dims;
+	}
+	
+	/*
 	 * Returns a boolean array of where the provided player can play their counters.
 	 */
 	public boolean[][] getLegalMoves(Player p) {
+		
 		boolean[][] legalMoves = new boolean[BOARD_SIZE][BOARD_SIZE];
+		
 		for (int row = 0; row < BOARD_SIZE; ++row) {
 			for (int col = 0; col < BOARD_SIZE; ++col) {
 				int boardValue = getBoardValue(row, col);
-				legalMoves[row][col] = false;
+				// Checks if board space is already occupied.
+				if (boardValue == 0) {
+					
+					// Determines if there is another counter within one space of the position.
+					boolean canMove = false;
+					for (int localRow = row - 1; localRow <= row + 1; ++localRow) {
+						for (int localCol = col - 1; localCol <= col + 1; ++localCol) {
+							if (localRow >= 0 && localRow < BOARD_SIZE && localCol >= 0 && localCol < BOARD_SIZE) {
+								if (this.board[localRow][localCol] != 0) {
+									canMove = true;
+								}
+							}
+						}
+					}
+					
+					legalMoves[row][col] = canMove;
+					
+				} else {
+					legalMoves[row][col] = false;
+				}
 			}
 		}
+		
 		return legalMoves;
 	}
-
+	
+	/*
+	 * Rotates the board 90 degrees clockwise for the number of times specified.
+	 * Directly changes the game state.
+	 */
+	private void rotateAndChange() {
+		int[][] oldBoard = this.getBoard();
+		for (int row = 0; row < BOARD_SIZE; ++row) {
+			for (int col = 0; col < BOARD_SIZE; ++col) {
+				this.board[row][col] = oldBoard[col][BOARD_SIZE - row];
+			}
+		}
+	}
+	
+	/*
+	 * Creates a GameState object that has been rotated by 90 degrees a 
+	 * certain number of times from this GameState.
+	 */
+	public GameState rotate(int numRotates) {
+		GameState newState = new GameState(this);
+		for (int i = 0; i < numRotates; ++i) {
+			newState.rotateAndChange();
+		}
+		return newState;
+	}
+	
+	/*
+	 * Checks to see if the provided game state is identical to this one.
+	 */
+	public boolean isEqual(GameState that) {
+		boolean result = true;
+		result = result && (this.hasSameBoardAs(that));
+		result = result && (this.getPlayer(0).equals(that.getPlayer(0)));
+		result = result && (this.getPlayer(1).equals(that.getPlayer(1)));
+		result = result && (this.turnNumber == that.getTurnNumber());
+		return result;
+		
+	}
+	
+	/*
+	 * Checks if a GameState is a rotation of another GameState.
+	 * Only checks the board of the game, not players or turn number.
+	 */
+	public boolean isRotationOf(GameState that) {
+		for (int rotation = 0; rotation < 4; ++rotation) {
+			GameState thatRotated = that.rotate(rotation);
+			if (this.hasSameBoardAs(thatRotated)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/*
+	 * Checks if two GameStates have identical boards.
+	 */
+	public boolean hasSameBoardAs(GameState that) {
+		int[] thatDims = that.getBoardDims();
+		if (BOARD_SIZE == thatDims[0]) {
+			int[][] thatBoard = that.getBoard();
+			for (int row = 0; row < BOARD_SIZE; ++row) {
+				for (int col = 0; col < BOARD_SIZE; ++col) {
+					if (this.board[row][col] != thatBoard[row][col]) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	/*
+	 * Method for easily displaying the game state.
+	 */
+	public String toString() {
+		
+		String divider = " -";
+		for (int i = 0; i < BOARD_SIZE; ++i) {
+			divider += "--";
+		}
+		
+		String theString = "Board:\n" + divider + "\n |";
+		
+		for (int row = 0; row < BOARD_SIZE; ++row) {
+			for (int col = 0; col < BOARD_SIZE; ++col) {
+				theString += this.board[row][col] + "|";
+			}
+			theString += "\n" + divider + "\n |";
+		}
+		
+		theString = theString.substring(0, theString.length() - 1) + "\n";
+		
+		theString += "Players:\n";
+		theString += " 1. " + this.players[0].toString() + "\n";
+		theString += " 2. " + this.players[1].toString() + "\n\n";
+		
+		theString += "Turn Number: " + this.turnNumber;
+		
+		return theString;
+		
+	}
+	
 }
--- src/main/Othello.java
+++ src/main/Othello.java
@@ -1,13 +1,25 @@
 package main;
 
 import game.GameState;
+import game.Player;
 
 public class Othello {
 	
-	private GameState game;
-
-	public Othello() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		GameState game = new GameState(new Player(), new Player());
+		System.out.println(game);
+		boolean[][] moves = game.getLegalMoves(game.getPlayer(0));
+		for (int row = 0; row < game.getBoardDims()[0]; ++row) {
+			for (int col = 0; col < game.getBoardDims()[1]; ++col) {
+				boolean canUse = moves[row][col];
+				if (canUse) {
+					System.out.print("T");
+				} else {
+					System.out.print("F");
+				}
+			}
+			System.out.println("");
+		}
 	}
 
 }
